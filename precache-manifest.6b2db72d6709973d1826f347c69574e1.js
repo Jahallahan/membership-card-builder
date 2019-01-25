04a353a4eec0372914f1c5c05c7f2f2e9a62dce8
jahallahan <hello@jackhallahan.com>
2019-01-25 16:09:06 +0800

    Updates

--- precache-manifest.6b2db72d6709973d1826f347c69574e1.js
+++ precache-manifest.6b2db72d6709973d1826f347c69574e1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/membership-card-builder/static/js/runtime~main.caad0579.js"
   },
   {
-    "revision": "16a73e9e207684122d03",
-    "url": "/membership-card-builder/static/js/main.16a73e9e.chunk.js"
+    "revision": "79c093c2409ba78482e3",
+    "url": "/membership-card-builder/static/js/main.79c093c2.chunk.js"
   },
   {
     "revision": "4d4949a9a9f903a5cc4a",
     "url": "/membership-card-builder/static/js/1.4d4949a9.chunk.js"
   },
   {
-    "revision": "16a73e9e207684122d03",
+    "revision": "79c093c2409ba78482e3",
     "url": "/membership-card-builder/static/css/main.123645f8.chunk.css"
   },
   {
-    "revision": "ecc521a636b2ab4a47970b81bfb4e1c0",
+    "revision": "198f33911494826b98a50e922b5dc423",
     "url": "/membership-card-builder/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/membership-card-builder/precache-manifest.1eabfc42d52759624f15e5f60f38afee.js"
+  "/membership-card-builder/precache-manifest.6b2db72d6709973d1826f347c69574e1.js"
 );
 
 workbox.clientsClaim();
